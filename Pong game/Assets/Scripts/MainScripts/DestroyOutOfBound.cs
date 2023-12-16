9f81dcb03005ea0c15bc89c74f12eb6a9f7f2959
mingfuc <phucfen@gmail.com>
2023-12-17 04:35:49 +0700

    update

--- Pong game/Assets/Scripts/MainScripts/DestroyOutOfBound.cs
+++ Pong game/Assets/Scripts/MainScripts/DestroyOutOfBound.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DestroyOutOfBound : MonoBehaviour
 {
     private float xBound = 24;
+    private float yBound = 14;
     private PointController pointControllerScript;
 
     // Start is called before the first frame update
@@ -17,14 +18,14 @@ public class DestroyOutOfBound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > xBound )
+        if (transform.position.x > xBound || transform.position.y > yBound)
         {
             pointControllerScript.p1Point++; //increase point
             pointControllerScript.continueSpawn = true;
             Destroy(gameObject);
             pointControllerScript.isBallSpawning = false;
         }
-        if (transform.position.x < -xBound)
+        if (transform.position.x < -xBound || transform.position.y < -yBound)
         {
             pointControllerScript.p2Point++;
             pointControllerScript.continueSpawn = true;
